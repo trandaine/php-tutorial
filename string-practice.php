48dee3972370fc489060cdcfcd69777d65dd5b75
Tran Dai <tranquangdai03072004@outlook.com.vn>
2024-04-08 22:29:34 +0700

    Bai tap lien quan toi string

--- string-practice.php
+++ string-practice.php
@@ -2,8 +2,20 @@
 
 /* Excercise 1; Create a program that will take 5 last characters of a string and display them. */
 
-$str="Hello World";
-$lastFiveChars = substr($str, -5);
-echo $lastFiveChars;
-echo "Five last characters: <br>";
+$str="Hello World";     // Provide the string to be processed
+$lastFiveChars = substr($str, -5);      // Get last five characters
+echo $lastFiveChars .'<br>';        // Display last five characters
+echo "Five last characters: <br>";      // Display message
 
+
+// Excercise 2: Create a program that will delete the first word of a string and display the rest of it.
+// Input: Tran Quang Dai
+// Output: Quang Dai
+
+$originalString = "Tran Quang Dai";        // Provide the string to be processed
+
+// Calculate the position of the first space
+$firstSpacePosition = strpos($originalString, ' ');        // Find the position of the first space          //Syntax: strpos('chuoi can xu li', 'ky tu can tim')
+$restOfString = substr($originalString, $firstSpacePosition + 1);        // Get the rest of the string      //Syntax: substr('chuoi can xu li', 'vi tri bat dau', 'do dai chuoi can cat')
+echo $restOfString;        // Display the rest of the string
+echo "<br>";
