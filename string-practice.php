a91810684ee7b7f9419a67f16b06f1032558ef3c
Tran Dai <tranquangdai03072004@outlook.com.vn>
2024-04-08 18:04:42 +0700

    Bai tap lien quan toi string

---
+++ string-practice.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Excercise 1; Create a program that will take 5 last characters of a string and display them. */
+
+$str="Hello World";
+$lastFiveChars = substr($str, -5);
+echo $lastFiveChars;
+echo "Five last characters: <br>";
+
