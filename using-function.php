7078d08591fca77a8e6934a9c258367053ea9d2f
Tran Dai <tranquangdai03072004@outlook.com.vn>
2024-04-09 10:23:33 +0700

    Su dung ham bang cach goi file extend

---
+++ using-function.php
@@ -0,0 +1,5 @@
+<?php
+
+include 'function.php';         // import function.php file to using function sum
+// This will make the function reusable in future projects
+echo sum(5, 10). '<br>'; // make total = 15
